66cf16eb484e84ac535a25b1413e89a30f2f0b33
Subrata Pal <subratapal0199@gmail.com>
2024-03-02 17:45:59 +0530

    Store procedure

--- ADO_dotnet.sql
+++ ADO_dotnet.sql
@@ -8,7 +8,7 @@ begin
 			set @mess='OMG! Finaly we got the record'
 	end
 	else
-		set @mess='OPPs! record not exits record'
+		set @mess='OPPs! record not exits'
 end
 
 alter proc [hr].usp_insertDeveloper(
@@ -33,10 +33,98 @@ as
 	end
 
 
-
 declare @mess varchar(100)
 exec [hr].usp_insertDeveloper 4,'Pallabi',30000,'Kolkata',@mess out
 print @mess
+-----------------------------------------------------------------------------------------------------------------
+
+create database ADO_Dot_net
+use ADO_Dot_net
+
+create table developer(
+						dev_id int identity primary key,
+						dev_name varchar(100),
+						dev_salary int
+						)
+
+INSERT INTO developer (dev_name, dev_salary) VALUES ('Subrata', 50000);
+INSERT INTO developer (dev_name, dev_salary) VALUES ('Rahul', 55000);
+INSERT INTO developer (dev_name, dev_salary) VALUES ('Pranav', 60000);
+INSERT INTO developer (dev_name, dev_salary) VALUES ('Susmita', 62000);
+INSERT INTO developer (dev_name, dev_salary) VALUES ('Subhrajit', 58000);
+
+
+alter proc usp_insertDeveloper(
+									@dev_name varchar(100),
+									@dev_salary int,
+									@identity int out,
+									@message varchar(100) out
+									)
+as 
+	begin
+			
+		insert into developer(dev_name,dev_salary) values
+		(@dev_name,@dev_salary);
+		set @identity=SCOPE_IDENTITY()
+		if @@ROWCOUNT>0
+			set @message='Record inserted successfully having id: '+cast(@identity as varchar(100))
+	end
+
+declare @mess varchar(100),@id int
+exec usp_insertDeveloper 'Pallabi',30000,@id out,@mess out
+print @mess
+print @id
+
+select * from developer
+
+---------------------------------------------------------------------------------------------------
+alter proc [hr].[usp_CrudApplication](
+									@dev_id int=null,
+									@dev_name varchar(100)=null,
+									@dev_salary int=null,
+									@location varchar(100)=null,
+									@message varchar(100) out
+									)
+as
+	begin
+		if not exists(select dev_id from HR.developer_tbl where dev_id=@dev_id)
+		begin
+			insert into [HR].[developer_tbl]([dev_id],[dev_name],[dev_salary],[location])values
+			(@dev_id,@dev_name,@dev_salary,@location)
+			if @@ROWCOUNT>0
+				set @message='Record inserted Successfully'
+			else
+				set @message='Record not inserted'
+			select dev_id,dev_name,dev_salary,[location]
+			from HR.developer_tbl
+		end
+	end
+
+alter proc [hr].[usp_CrudApplication](
+									@dev_id int,
+									@dev_name varchar(100),
+									@message varchar(100) out
+								)
+as
+begin
+		
+			select dev_id,dev_name,dev_salary,[location]
+			from HR.developer_tbl
+			where dev_id=@dev_id or dev_name=@dev_name
+			if @@ROWCOUNT>0
+				set @message='Here is your result'
+			else
+				set @message='No record found'
+end
+
+
+
+declare @mess varchar(100)
+exec [hr].[usp_CrudApplication] 1,'Avik',@mess out 
+print @mess
+
+
+
 
 
 
